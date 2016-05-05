28ee0293bac65d73a00b735057eda3704c4d4590
zhangkari <zhangkari.mail@gmail.com>
2016-05-05 20:57:29 +0800

    Study gluLookAt

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,10 @@
 #include "RendererFactory.h"
 
 Renderer *gRenderer = NULL;
+Bitmap *gBitmap = NULL;
 
 void init(int argc, char* argv[]) {
+	glClearColor(0.0, 0.0, 0.0, 0.0);
 	gRenderer = RendererFactory::createRenderer(TYPE_REALTIME);
 	if (NULL == gRenderer) {
 		printf("Failed create renderer.\n");
@@ -17,16 +19,38 @@ void init(int argc, char* argv[]) {
 	}
 
 	if (2 == argc) {
-		Bitmap *bmp = Bitmap::load(argv[1]);
-		if (NULL == bmp) {
+		gBitmap = Bitmap::load(argv[1]);
+		if (NULL == gBitmap) {
 			printf("Failed load %s\n", argv[1]);
 		} else {
-			gRenderer->setArtwork(bmp);
+			gRenderer->setArtwork(gBitmap);
 		}
 	}
 }
 
+void onReshape(int w, int h) {
+	printf("onReshape()\n");
+	glViewport(0, 0, (GLsizei)w, (GLsizei) h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(60, (GLfloat)w/(GLfloat)h, 1.0, 20.0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(0.0, 0.0, 5.0, 0.0, 0.0, 0.0, -1.0, 0.0, 0.0);
+}
+
 void onDisplay() {
+	printf("onDisplay()\n");
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(1.0, 1.0, 1.0);
+
+	glutWireTeapot(1.5);
+//	glutSolidTeapot(2);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(0.0, 0.0, 5.0, 0.0, 0.0, 0.0, -1.0, 0.0, 0.0);
+
 	if (NULL != gRenderer) {
 		gRenderer->render();
 	}
@@ -45,6 +69,13 @@ void onNormalKeyClicked(unsigned char key, int x, int y) {
 
 	} else if (key == 'e') {
 		glutReshapeWindow(600, 600);
+
+	}
+}
+
+void onSpecialKeyClicked(int key, int x, int y) {
+	if (key == GLUT_KEY_UP) {
+		printf("up\n");
 	}
 }
 
@@ -57,7 +88,9 @@ int main(int argc, char *argv[])
 	glutCreateWindow("Opengl View");
 	init(argc, argv);
 	glutDisplayFunc(onDisplay);
+	glutReshapeFunc(onReshape);
 	glutKeyboardFunc(onNormalKeyClicked);
+	glutSpecialFunc(onSpecialKeyClicked);
 	glutMainLoop();
 	return 0;
 }
